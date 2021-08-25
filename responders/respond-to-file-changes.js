60f4856b4344eaef381e6c5c89ce2b1a74e41a25
Jim Kang <jimkang@gmail.com>
2021-08-25 11:51:47 -0400

    Remove unused file.

--- responders/respond-to-file-changes.js
+++
@@ -1,6 +0,0 @@
-import handleError from 'handle-error-web';
-
-export function respondToFileChanges({ files, onBuffers }) {
-  var bufferPromises = files.map((file) => file.arrayBuffer());
-  Promise.all(bufferPromises).then(onBuffers).catch(handleError);
-}
